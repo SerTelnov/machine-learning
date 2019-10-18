c2b3bcb8600c2d05dbe41873e2d06e5867cc1163
Sergei Telnov <sergeitelnov@yandex-team.ru>
2019-10-18 12:00:49 +0300

    add schedules

--- labs/kNN/drawer.py
+++ labs/kNN/drawer.py
@@ -1,40 +1,50 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+import core
+import data
+
 
 class kNNWinner:
-    def __init__(self, kernel_name, dist_name, window_option):
+    def __init__(self, kernel_name, dist_name, is_fixed):
         self.kernel_name = kernel_name
         self.dist_name = dist_name
-        self.window_option = window_option
+        self.is_fixed = is_fixed
 
-    def draw(self, steps):
+    def draw(self, X, Y):
+        all_dists, max_dist = core.count_dists(X, core.get_dist_func(self.dist_name))
+        win_step = max_dist / 500
+
+        steps = np.arange(0, 0.5, win_step) if self.is_fixed else range(1, 100)
         f_measure = []
-        steps = range(1, 15)
-        plt.plot(f_measure, steps)
+
+        for curr_win in steps:
+            f = core.brute_force_params(curr_win, all_dists, core.get_kernel_func(self.kernel_name), self.is_fixed, Y)
+            f_measure.append(f)
+
+        plt.plot(steps, f_measure)
 
         plt.suptitle("kernel: '" + self.kernel_name + "' dist_name: '" + self.dist_name + "'")
-        plt.xlabel('f measure')
-        plt.ylabel('window steps')
+        plt.ylabel('f measure')
+        plt.xlabel('window steps')
 
         plt.show()
 
 
 def read_options(win_option):
-    print("tipe fixed options")
-
     print("kernel_name")
     kernel_name = input()
 
     print("dist_name")
     dist_name = input()
 
-    print("f_measure")
-    f_measure = (float(f) for f in input().split())
-    return kNNWinner(kernel_name, dist_name, win_option, f_measure)
+    return kNNWinner(kernel_name, dist_name, win_option)
 
-fixed_options = read_options("fixed")
-variable_options = read_options("variable")
 
-fixed_options.draw()
-variable_options.draw()
+X, Y = data.read_data("labs/kNN/iris.csv")
+
+print("type option (fixed/variable)")
+is_fixed = input() == "fixed"
+
+option = read_options(is_fixed)
+option.draw(X, Y)
