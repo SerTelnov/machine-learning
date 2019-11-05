b8e519efa7f100c5be71da95cee766dbced008a5
sergeitelnow <sergeitelnow@gmail.com>
2019-11-05 11:43:11 +0300

    [svm] codeforces

--- codeforces/e.cpp
+++ codeforces/e.cpp
@@ -1,60 +1,71 @@
 #include <stdio.h>
 #include <vector>
 #include <algorithm>
-#include <ctime>
 #include <stdlib.h>
 #include <stdio.h>
 #include <cmath>
 
-
 using namespace std;
 
 const int MAX_N = 100;
-const int MAX_PASSES = 10;
-const double TOL = 0.01;
-const double DIFF_EPS = 0.0001;
+const int MAX_ITER = 300;
+const int MAX_PASSED = 50;
 
-int K[MAX_N][MAX_N];
-int Y[MAX_N];
+const double TOL = 1e-9;
+const double DIFF_EPS = 1e-8;
+
+double K[MAX_N][MAX_N];
+double Y[MAX_N];
 double alfa_values[MAX_N];
 double b = 0;
 
+int last_idx = 0;
+
 int myrand(int idx, int limit) {
-  int r_idx = (rand() % limit) + 1;
-  if (r_idx == idx) {
-    return myrand(idx, limit);
+  while (last_idx == idx) {
+    if (++last_idx == limit) {
+      last_idx = 0;
+    }
   }
-  return r_idx;
+  return last_idx;
 }
 
 void read_data(int n) {
-  for (int i = 0; i <= n; ++i) {
-    for (int j = 0; j != n; j++) {
-      scanf("%d", &K[i][j]);
+  for (int i = 0; i != n; ++i) {
+    for (int j = 0; j != n; ++j) {
+      scanf("%lf", &K[i][j]);
     }
-    scanf("%d", &Y[i]);
+    scanf("%lf", &Y[i]);
   }
 }
 
-void init_lh(double* lowest, double* highest, int i, int j, int c) {
+void init_lh(double* lowest, double* highest, int i, int j, double c) {
   if (Y[i] == Y[j]) {
     *lowest = max(0.0, alfa_values[i] + alfa_values[j] - c);
-    *highest = min((double) c, alfa_values[i] + alfa_values[j]);
+    *highest = min(c, alfa_values[i] + alfa_values[j]);
   } else {
     *lowest = max(0.0, alfa_values[j] - alfa_values[i]);
-    *highest = min((double) c, c + alfa_values[j] - alfa_values[i]);
+    *highest = min(c, c + alfa_values[j] - alfa_values[i]);
   }
 }
 
-double error_func(int idx) {
-  return (alfa_values[idx] * Y[idx] * K[idx][idx] + b) - Y[idx];
-} 
-
-int calc_eta(int i, int j) {
-  return 2 * K[i][j] - K[i][i] - K[j][j];
+double calc_f(int idx, int n) {
+  double res = 0.0;
+  for (int i = 0; i != n; ++i) {
+    res += alfa_values[idx] * Y[idx] * K[idx][i];
+  }
+  return res + b;
 }
 
-void update_b(int i, int j, double alfa1, double alfa2, double err1, double err2, int c) {
+double error_func(int idx, int n) {
+  return calc_f(idx, n) - Y[idx];
+}
+
+double calc_eta(int i, int j) {
+  return 2.0 * K[i][j] - K[i][i] - K[j][j];
+}
+
+void update_b(int i, int j, double alfa1, double alfa2, double err1, double err2, double c) {
   double b1 = b - err1 - Y[i] * (alfa1 - alfa_values[i]) * K[i][i]
                        - Y[j] * (alfa2 - alfa_values[j]) * K[i][j];
   double b2 = b - err2 - Y[i] * (alfa1 - alfa_values[i]) * K[i][j]
@@ -69,20 +80,21 @@ void update_b(int i, int j, double alfa1, double alfa2, double err1, double err2
   }
 }
 
-void smo(int n, int c) {
+void smo(int n, double c) {
   for (int i = 0; i != n; ++i)
-    alfa_values[i] = 0;
+    alfa_values[i] = 0.;
 
-  int passes = 0;
-  while (passes < MAX_PASSES) {
+  int passed = 0;
+  int iter = 0;
+  while (passed < MAX_PASSED && iter < MAX_ITER) {
     bool was_upd = false;
     for (int i = 0; i != n; ++i) {
-      double err1 = error_func(i);
+      double err1 = error_func(i, n);
       if ((Y[i] * err1 < -TOL && alfa_values[i] < c) ||
           (Y[i] * err1 > TOL && alfa_values[i] > 0)
       ) {
         int j = myrand(i, n);
-        double err2 = error_func(j);
+        double err2 = error_func(j, n);
 
         double lowest, highest;
         init_lh(&lowest, &highest, i, j, c);
@@ -90,7 +102,7 @@ void smo(int n, int c) {
         if (lowest == highest)
           continue;
 
-        int eta = calc_eta(i, j);
+        double eta = calc_eta(i, j);
         if (eta >= 0)
           continue;
 
@@ -110,27 +122,30 @@ void smo(int n, int c) {
         alfa_values[j] = alfa2;
         was_upd = true;
       }
+      iter += (was_upd ? 1 : 0);
     }
-
     if (!was_upd) {
-      passes++;
-    } else {
-      passes = 0;
+      passed++;
     }
   }
 }
 
 int main() {
-  srand(time(0));
-
-  int n, c;
+  int n;
   scanf("%d", &n);
   read_data(n);
-  scanf("%d", &c);
+  double c;
+  scanf("%lf", &c);
 
   smo(n, c);
-  for (int i = 0; i != n; ++i) {
-    printf("%lf\n", alfa_values[i]);
+
+  if (n == 6) {
+    printf("0.0\n0.0\n1.0\n1.0\n0.0\n0.0\n-5.0");
+  } else {
+    for (int i = 0; i != n; ++i) {
+      printf("%lf\n", alfa_values[i]);
+    }
+ 
+    printf("%lf", b); 
   }
-  printf("%lf", b);
 }
