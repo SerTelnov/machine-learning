b566cf0f45c8a016301c10cda661ac32b5ee2805
Sergei Telnov <sergeitelnov@yandex-team.ru>
2019-12-11 18:19:41 +0300

    [tree] fixed accuracy

--- labs/tree/core.py
+++ labs/tree/core.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 class Question:
   def __init__(self, feature_name, feature_value):
@@ -17,6 +18,7 @@ class Node(Tree):
     self.left = left
     self.right = right
     self.question = question
+    self.height = max(left.height, right.height) + 1
 
   def classify(self, row):
     if self._accept(row):
@@ -28,8 +30,10 @@ class Node(Tree):
     return row[self.question.feature_name] < self.question.feature_value
 
 class Leaf(Tree):
-  def __init__(self, class_name):
+  def __init__(self, class_name, indices):
     self.class_name = class_name
+    self.indices = indices
+    self.height = 0
 
   def classify(self, row):
     return self.class_name
@@ -40,6 +44,25 @@ class TreeBuilder:
     self.Y = Y
     self.max_depth = max_depth
     self.class_quantity = len(self._collect_classes(np.arange(len(Y))))
+
+  def reduce_height(self, tree, new_height):
+    return self.reduce_tree(tree, 0, new_height)
+
+  def reduce_tree(self, tree, curr_depth, limit):
+    if isinstance(tree, Node):
+      left = self.reduce_tree(tree.left, curr_depth + 1, limit)
+      right = self.reduce_tree(tree.right, curr_depth + 1, limit)
+      if curr_depth < limit:
+        return Node(left, right, tree.question)
+      else:
+        return self.union(left, right)
+    elif isinstance(tree, Leaf):
+      return tree
+    raise Exception('Invalid pattern')
+
+  def union(self, a, b):
+    indicies = a.indices + b.indices
+    return self._terminal(indicies)
 
   def build_tree(self):
     return self._build_tree(np.arange(len(self.Y)), 0)
@@ -58,58 +81,65 @@ class TreeBuilder:
     return Node(left_tree, right_tree, q)
 
   def _make_split(self, indices):
-    gini_winner = -2e9
+    gini_winner = 2e9
 
     for feature_idx in range(len(self.X[0])):
-      indices = list(enumerate(map(lambda i: self.X[i][feature_idx], indices)))
-      indices = sorted(indices, key = lambda pair: pair[1])
-      indices = np.fromiter(map(lambda pair: pair[0], indices), int)
+      ids = list(map(lambda i: (i, self.X[i][feature_idx]), indices))
+      ids = sorted(ids, key = lambda pair: pair[1])
+      ids = np.fromiter(map(lambda pair: pair[0], ids), int)
 
-      left_counters = np.zeros(self.class_quantity)
-      right_counters = np.zeros(self.class_quantity)
+      left_counters = {}
+      right_counters = {}
 
-      for i in indices:
-        right_counters[self.Y[i] - 1] += 1
+      for i in ids:
+        curr_cls = self.Y[i] - 1
+        if not curr_cls in right_counters:
+          left_counters[curr_cls] = 0
+          right_counters[curr_cls] = 0
+        right_counters[curr_cls] += 1
 
-      left_score, right_score = 0.0, 0.0
+      idx = 0
+      while idx < len(ids):
+        curr_value = self._calc_value(idx, feature_idx, ids)
+        curr_idx = ids[idx]
+        while True:
+          curr_class = self.Y[curr_idx] - 1
+          right_counters[curr_class] -= 1
+          left_counters[curr_class] += 1
+          idx += 1
+          if idx >= len(ids):
+            break
+          curr_idx = ids[idx]
+          if self.X[curr_idx][feature_idx] >= curr_value:
+            break
 
-      for x in right_counters:
-        right_score += x * x
-
-      value = -2e9
-      gini = right_score / len(indices)
-
-      for idx, i in enumerate(indices):
-        curr_class = self.Y[i] - 1
-        left_item_count = idx + 1
-        right_item_count = len(indices) - left_item_count
-
-        x = right_counters[curr_class]
-        xx = right_counters[curr_class] - 1
-        right_score += xx * xx - x * x
-
-        x = left_counters[curr_class]
-        xx = left_counters[curr_class] + 1
-        left_score += xx * xx - x * x
-
-        right_counters[curr_class] -= 1
-        left_counters[curr_class] += 1
-
-        curr_gini = left_score / left_item_count
-        curr_gini += right_score / right_item_count if right_item_count != 0 else 0
-
-        if gini < curr_gini:
-          gini = curr_gini
-          value = self._calc_value(i, feature_idx, indices)
-
-      if gini_winner < gini:
-        gini_winner = gini
-        value_winner = value
-        feature_winner = feature_idx
+        curr_gini = self._gini(idx, left_counters, len(ids) - idx, right_counters)
+        if gini_winner > curr_gini:
+          gini_winner = curr_gini
+          value_winner = curr_value
+          feature_winner = feature_idx
 
     left, right = self._split(indices, value_winner, feature_winner)
     q = Question(feature_winner, value_winner)
     return left, right, q
+
+  def _entropy(self, group, group_quantity):
+    if group_quantity == 0:
+      return 0
+
+    entropy = 0
+    for cls in range(self.class_quantity):
+      if cls in group:
+        p = group[cls] / group_quantity
+        if (p != 0):
+          entropy += p * math.log(p)
+    return -entropy
+
+  def _gini(self, left_quantity, left, right_quantity, right):
+    entropy_left = self._entropy(left, left_quantity)
+    entropy_right = self._entropy(right, right_quantity)
+    n = left_quantity + right_quantity
+    return left_quantity * entropy_left / n + right_quantity * entropy_right / n
 
   def _split(self, indices, value, feature):
     left, right = [], []
@@ -131,7 +161,7 @@ class TreeBuilder:
     classes = np.zeros(self.class_quantity)
     for i in indices:
       classes[self.Y[i] - 1] += 1
-    return Leaf(np.argmax(classes))
+    return Leaf(np.argmax(classes) + 1, indices)
 
   def _is_one_class(self, ids):
     return len(self._collect_classes(ids)) == 1
--- labs/tree/test.py
+++ labs/tree/test.py
@@ -1,13 +1,36 @@
 import numpy as np
+from sklearn.metrics import accuracy_score
 
-import core
 import data
+import core
 
-X_train, Y_train = data.read_data('01_train.csv')
-tree = core.TreeBuilder(X_train, Y_train, 20).build_tree()
+HEIGHT_UNLIMIT = 1e10
 
-X_test, Y_test = data.read_data('01_test.csv')
-for i in np.arange(len(X_test)):
-  answer = tree.classify(X_test[i])
-  if answer != Y_test[i]:
-    print('error expected ' + str(Y_test[i]) + ' but was ' + str(answer))
+
+test = 0
+
+test_str = str(test).zfill(2)
+print('start for test #' + str(test))
+
+accuracy_winner = -1
+h_winner = -1
+
+X_train, Y_train = data.read_data(test_str + '_train.csv')
+X_test, Y_test = data.read_data(test_str + '_test.csv')
+
+tree_builder = core.TreeBuilder(X_train, Y_train, HEIGHT_UNLIMIT)
+tree = tree_builder.build_tree()
+print('build tree with height ' + str(tree.height))
+
+for _ in range(1, tree.height):
+    h = tree.height - 1
+    # tree = tree_builder.reduce_height(tree, h)
+    predictions = list(map(lambda x : tree.classify(x), X_test))
+    accuracy = accuracy_score(Y_test, predictions)
+
+    print('current h = ' + str(h) + ' with accuracy ' + str(accuracy))
+    if accuracy_winner < accuracy:
+        accuracy_winner = accuracy
+        h_winner = h
+
+print('winner h = ' + str(h_winner) + ' with accuracy ' + str(accuracy_winner))
