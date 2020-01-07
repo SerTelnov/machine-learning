8d4fc2a2b9e8c8ac8e0118850d2af474497184f4
Sergei Telnov <sergeitelnov@yandex-team.ru>
2020-01-07 18:13:51 +0100

    [cf] F solved

--- codeforces/f.py
+++ codeforces/f.py
@@ -16,22 +16,9 @@ def _safe_parse_msg(msg, idx):
         return msg[idx:]
     return []
 
-def _safe_get(cur_dict, key):
-    if not key in cur_dict:
-        return 0
-    return cur_dict[key]
-
 def _safe_log(x):
     return math.log(x) if x != 0 else 0
 
-def _safe_exp(x):
-    return math.exp(x)
-    # return math.exp(x) if abs(x) > (1e-2) else 1
-
-def _safe_div(x, d):
-    if x == 0:
-        return 0
-    return _safe_exp(_safe_log(x) - _safe_log(d))
 
 def read_data():
     input()
@@ -62,7 +49,7 @@ def train(msgs, clazz_fine):
         clazz_msgs[clazz] += 1
 
         if not msg.words:
-            words_msg_counts[clazz] = []
+            words_msg_counts[clazz] = {}
 
         for word in set(msg.words):
             _inc(words_msg_counts, clazz, word)
@@ -73,30 +60,34 @@ def train(msgs, clazz_fine):
         if not clazz in words_msg_counts:
             words_msg_counts[clazz] = {}
 
-        x = clazz_msgs[clazz] / len(msgs) * clazz_fine[clazz]
-        if x != 0:
-            preparetion[clazz] = ClazzInfo(math.log(x), words_msg_counts[clazz], clazz_msgs[clazz])
+        if clazz_msgs[clazz] != 0:
+            x = math.log(clazz_msgs[clazz] / len(msgs) * clazz_fine[clazz])
+            preparetion[clazz] = ClazzInfo(x, words_msg_counts[clazz], clazz_msgs[clazz])
 
     return preparetion
 
 def classifier(preparetion, n, alfa, words):
-    scores = []
+    scores = [0] * n
 
     for clazz in range(n):
         score = 0
         if clazz in preparetion:
             info = preparetion[clazz]
             for word in set(words):
-                x = alfa + _safe_get(info.words_msg_count, word)
+                x = alfa + (info.words_msg_count.get(word) or 0)
                 y = info.msg_count + alfa * len(info.words_msg_count)
                 if x != 0 and y != 0:
                     score += math.log(x) - math.log(y)
             score += info.probability
-            score = _safe_exp(score)
-        scores.append(score)
+        scores[clazz] = score
+
+    maxq = max(scores)
+    for i in range(n):
+        scores[i] -= maxq
+        scores[i] = math.exp(scores[i]) if i in preparetion else 0
 
     scores_sum = sum(scores)
-    print(' '.join(map(str, map(lambda x:  _safe_div(x, scores_sum), scores))))
+    print(' '.join(map(str, map(lambda x:  x / scores_sum, scores))))
 
 clazz_fine, alfa, train_msgs = read_data()
 preparetion = train(train_msgs, clazz_fine)
